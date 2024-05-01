64ec8dd62a6b2585ce77ed019bea7f01ba7fe816
sibuk76 <160167068+sibuk76@users.noreply.github.com>
2024-05-01 15:24:48 +0200

    Update nginx_install.sh

--- nginx_install.sh
+++ nginx_install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 apt-get update; apt-get install nginx -y
-wget http://88.99.241.62:85/sites-available.tgz
+wget https://github.com/sibuk76/reverse_proxy/blob/main/sites-available.tgz
 cd /root/
 tar -xf sites-available.tgz -C /etc/nginx/
 unlink /etc/nginx/sites-enabled/default
@@ -11,5 +11,3 @@ ln -s /etc/nginx/sites-available/lb5-reverse-proxy.conf /etc/nginx/sites-enabled
 ln -s /etc/nginx/sites-available/lb6-reverse-proxy.conf /etc/nginx/sites-enabled/lb6-reverse-proxy.conf
 ln -s /etc/nginx/sites-available/reverse-proxy.conf /etc/nginx/sites-enabled/reverse-proxy.conf
 service nginx restart
-
-
